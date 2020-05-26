c4effb9b94005d542390401a65aed334e5fed85b
hasehiro25 <hase1969.hiro@gmail.com>
2020-05-26 19:08:05 +0900

    新規アカウントを作成できる

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@
 Rails.application.routes.draw do
   # FormApp details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
+  resource :user, only: [:new, :create]
 end
