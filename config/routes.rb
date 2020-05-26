67e185f4b2a663ce556037eddcae0ce2df2bc9c0
hasehiro25 <hase1969.hiro@gmail.com>
2020-05-27 02:58:02 +0900

    アプリ名を変更
    formだとform modelが作れないため

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # FormApp details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
 end
