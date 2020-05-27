3af21c20cac1a7930bdb8842c1f8b5b128955dd8
hasehiro25 <hase1969.hiro@gmail.com>
2020-05-27 16:43:47 +0900

    アクティベーショメールの単体テスト追加

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,32 +1,35 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "activation_needed_email" do
-    let(:mail) { UserMailer.activation_needed_email }
+    let(:user) { create(:user) }
+    let(:mail) { UserMailer.activation_needed_email(user) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Activation needed email")
-      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.subject).to eq("Formへのご登録ありがとうございます")
+      expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(["from@example.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match("アカウント登録ありがとうございます。")
     end
   end
 
   describe "activation_success_email" do
-    let(:mail) { UserMailer.activation_success_email }
+    let(:user) { create(:user) }
+    let(:mail) { UserMailer.activation_success_email(user) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Activation success email")
-      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.subject).to eq("Formのアカウントを有効にしました")
+      expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(["from@example.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match("アカウントを有効にしました。")
     end
   end
-
 end
