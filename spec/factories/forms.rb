07ef05e98830375eb3ff1f4150d297b62b46e9e0
hasehiro25 <hase1969.hiro@gmail.com>
2020-05-29 02:57:41 +0900

    フォーム作成時の単体テスト追加

--- spec/factories/forms.rb
+++ spec/factories/forms.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :form do
-    
+    name { "sampleform" }
+    domain { "example.com" }
+    redirect_url { "https://exampl.com/thankyou" }
   end
 end
--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -1,5 +1,17 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe Form, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "when form is created" do
+    let(:user) { create(:activated_user) }
+    let(:form) { build(:form, user: user) }
+    it "should save form" do
+      expect(form.save).to eq true
+    end
+    it "should have uuid in endpoint_id" do
+      form.save
+      expect(form.endpoint_id).to match(/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/)
+    end
+  end
 end
