2577341921fcacb3814503934aaad7a9ff17ec6a
hasehiro25 <hase1969.hiro@gmail.com>
2020-05-31 03:03:40 +0900

    問い合わせ内容の転送メールの単体テスト追加

--- spec/mailers/inquiry_mailer_spec.rb
+++ spec/mailers/inquiry_mailer_spec.rb
@@ -1,5 +1,27 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe InquiryMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "inquery_info" do
+    data = {
+      "名前": "太郎",
+      "内容": "こんにちは"
+    }
+
+    let(:user) { create(:activated_user) }
+    let(:form) { create(:form, user: user) }
+    let(:mail) { InquiryMailer.with(user: user, data: data, form: form).inquiry_info }
+
+    it "renders the header" do
+      expect(mail.subject).to eq("お問い合わせがありました")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["from@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("#{form.name}にお問い合わせがありました")
+      expect(mail.body.encoded).to match("<dd><p>太郎</p></dd>")
+    end
+  end
 end
