cc1287b40752f494ee627b7b0a635630f577bc7e
hasehiro25 <hase1969.hiro@gmail.com>
2020-06-01 21:08:04 +0900

    半角数字のvalidation追加

--- public/form_lib.js
+++ public/form_lib.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const errors = document.querySelectorAll("[class^=fform-js-error]")
   console.log(errors)
-  const blank_validations = document.querySelectorAll(".fform-js-blank-validation");
+  const blank_validations = document.querySelectorAll(".fform-js-blank-validation")
+  const email_validations = document.querySelectorAll(".fform-js-numeric-validation")
   const submit = document.querySelector(".fform-js-submit")
   let sendable = true
   for (const error of errors) {
@@ -24,10 +25,27 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   }
+  //numeric validation
+  let validateNumeric = () => {
+    for (const elms of email_validations) {
+      const parent = elms.closest("div")
+      const error = parent.querySelector(".fform-js-error-numeric")
+      if (!Number.isInteger(parseInt(elms.value))) {
+        // error.style.visibility = "visible"
+        error.style.display = "block"
+        sendable = false
+      } else {
+        error.style.display = "none"
+        // error.style.visibility = "hidden"
+      }
+    }
+  }
+
   submit.addEventListener('click', (e) => {
     // e.preventDefault()
     sendable = true
     blank_validator()
+    validateNumeric()
     if (sendable != true) {
       e.preventDefault()
     }
