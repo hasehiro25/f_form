dd27595d837e5cc78cdea325c123dc4cf8b4ca55
hasehiro25 <hase1969.hiro@gmail.com>
2020-06-05 02:24:34 +0900

    全角文字にvalidationをかける

--- public/form_lib.js
+++ public/form_lib.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const errors = document.querySelectorAll("[class^=fform-js-error]")
-  console.log(errors)
   const blank_validations = document.querySelectorAll(".fform-js-blank-validation")
   const email_validations = document.querySelectorAll(".fform-js-numeric-validation")
+  const fullwidthCharValidations = document.querySelectorAll(".fform-js-fullwidth-char-validation")
   const submit = document.querySelector(".fform-js-submit")
   let sendable = true
+
   for (const error of errors) {
     error.style.display = "none"
     // error.style.visibility = "hidden"
@@ -25,6 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   }
+
   //numeric validation
   let validateNumeric = () => {
     for (const elms of email_validations) {
@@ -41,11 +43,27 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  let validateFullwidthChar = () => {
+    for (const elms of fullwidthCharValidations) {
+      const parent = elms.closest("div")
+      const error = parent.querySelector(".fform-js-error-fullwidth-char")
+      const regex = /^[a-zA-Z0-9!-/:-@¥[-`{-~\]]*$/
+      if (!regex.test(elms.value)) {
+        // error.style.visibility = "visible"
+        error.style.display = "block"
+        sendable = false
+      } else {
+        error.style.display = "none"
+        // error.style.visibility = "hidden"
+      }
+    }
+  }
+
   submit.addEventListener('click', (e) => {
-    // e.preventDefault()
     sendable = true
     blank_validator()
     validateNumeric()
+    validateFullwidthChar()
     if (sendable != true) {
       e.preventDefault()
     }
