10a22e5be95e342340adca0810197b9f17565c71
hasehiro25 <hase1969.hiro@gmail.com>
2020-06-10 15:17:14 +0900

    recaptchaのバックエンド実装

--- app/controllers/api/v1/forms_controller.rb
+++ app/controllers/api/v1/forms_controller.rb
@@ -11,6 +11,10 @@ class Api::V1::FormsController < ApplicationController
     @form = Form.find_by!(endpoint_id: params[:id])
     raise "unauthorized domain" if unauthorized_domain?
     data = request.request_parameters.to_h
+    if data.has_key?("g-recaptcha-response")
+      check_recaptcha(data["g-recaptcha-response"], @domain)
+      data.delete("g-recaptcha-response")
+    end
     @form.inquiries.create!(content: data.to_json)
     InquiryMailer.with(form: @form, user: @form.user, data: data).inquiry_info.deliver_now
     @form.redirect_url? ? redirect_to(@form.redirect_url) : redirect_to(thankyou_url)
@@ -22,7 +26,12 @@ class Api::V1::FormsController < ApplicationController
     end
 
     def authorized_domain?
-      domain = request.headers[:HTTP_ORIGIN].gsub(/(http|https):\/\//, "")
-      domain == @form.domain
+      @domain ||= request.headers[:HTTP_ORIGIN].gsub(/(http|https):\/\//, "")
+      @domain == @form.domain
+    end
+
+    def check_recaptcha(token, registered_domain)
+      recaptcha_checker =  RecaptchaChecker.new({ token: token, registered_domain: registered_domain })
+      raise "unauthorized" unless recaptcha_checker.valid_token?
     end
 end
