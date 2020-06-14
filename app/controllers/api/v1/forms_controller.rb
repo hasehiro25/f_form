4bd0cb45d1e690e616c06c4e8d9b5ab9391cd8bd
hasehiro25 <hase1969.hiro@gmail.com>
2020-06-15 01:18:22 +0900

    必要のないインスタンス変数とメソッドを削除

--- app/controllers/api/v1/forms_controller.rb
+++ app/controllers/api/v1/forms_controller.rb
@@ -8,22 +8,18 @@ class Api::V1::FormsController < ApplicationController
   end
 
   def create
-    @form = Form.find_by!(endpoint_id: params[:id])
-    authorize_domain(@form.domain)
+    form = Form.find_by!(endpoint_id: params[:id])
+    authorize_domain(form.domain)
     data = request.request_parameters.to_h
-    process_recaptcha(data, domain,) if data.has_key?("g-recaptcha-response") || @form.recaptcha_status?
-    @form.inquiries.create!(content: data.to_json)
-    InquiryMailer.with(form: @form, user: @form.user, data: data).inquiry_info.deliver_now
-    @form.redirect_url? ? redirect_to(@form.redirect_url) : redirect_to(thankyou_url)
+    process_recaptcha(data, domain,) if data.has_key?("g-recaptcha-response") || form.recaptcha_status?
+    form.inquiries.create!(content: data.to_json)
+    InquiryMailer.with(form: form, user: form.user, data: data).inquiry_info.deliver_now
+    form.redirect_url? ? redirect_to(form.redirect_url) : redirect_to(thankyou_url)
   end
 
   private
     def authorize_domain(form_domain)
       raise "unauthorized domain" unless domain == form_domain
-    end
-
-    def authorized_domain?
-      domain == @form.domain
     end
 
     def domain
