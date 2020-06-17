fb85ebf44662dd4e4779c3223b93ece487655421
hasehiro25 <hase1969.hiro@gmail.com>
2020-06-17 23:48:35 +0900

    url生成のヘルパーメソッドを分ける

---
+++ app/helpers/url_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module UrlHelper
+  def endpoint_url(endpoint_id)
+    "#{root_url}api/v1/forms/#{endpoint_id}"
+  end
+
+  def fform_core_js_url
+    "#{root_url}/form_lib.js"
+  end
+end
