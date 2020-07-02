1d5965505b70a3569239777483f9a2223425de02
hasehiro25 <hase1969.hiro@gmail.com>
2020-07-03 02:16:26 +0900

    いらないファイルの削除

--- spec/support/authentication_contexts.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-shared_context "ログインしている" do |e|
-  let(:user) { create :activated_user }
-
-  before do
-    post "/login", params: { user: { email: user.email, password: "hogehoge" } }
-  end
-end
