56843ca669f3f85328716df684058c92dd49a743
hasehiro25 <hase1969.hiro@gmail.com>
2020-07-07 03:27:58 +0900

    パスワードの最低長さを６に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   VALID_EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\Z/
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :email, uniqueness: true, presence: true, format: { with: VALID_EMAIL_REGEX }
