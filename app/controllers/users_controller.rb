2e5fb9a92f171215199b657b386b67eb5bfb45a9
hasehiro25 <hase1969.hiro@gmail.com>
2020-07-06 22:16:22 +0900

    フラッシュメッセージを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path, notice: "アカウントを作成しました"
+      redirect_to login_path, notice: "認証メールを送信しました"
     else
       render "signup/new"
     end
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def activate
     if @user = User.load_from_activation_token(params[:id])
       @user.activate!
-      redirect_to(login_path, notice: "アカウントを作成しました")
+      redirect_to login_path, notice: "アカウントを作成しました"
     else
       not_authenticated
     end
